deaa1fc5aa446eb5f18ef690019047276b881138
sarvagyashiva0-web <sarvagyashiva0@gmail.com>
2025-09-20 18:47:00 +0530

    Add verifyAdmin middleware and fix import

---
+++ middleware/auth.js
@@ -0,0 +1,9 @@
+const verifyAdmin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(403).json({ error: 'Access denied. Admins only.' });
+  }
+};
+
+module.exports = { verifyAdmin };
